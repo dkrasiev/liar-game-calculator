28855b87819b07ff84141ce7ab7e1822ee33756e
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2023-08-09 22:34:56 +0400

    build: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { name } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/" + name,
+});
